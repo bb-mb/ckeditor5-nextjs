fceb527baa3f2ff8a541b56ad3c7fc240c77d753
soso01 <6125024@gmail.com>
2022-05-04 09:10:46 +0900

    <feat> editor 설치

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  experimental: {
+    reactMode: "concurrent",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
